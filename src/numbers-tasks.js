c6b9c6edeb2cd4f70bd8e712376f2e59a63f447c
LVJameskirk <LVJameskirk@gmail.com>
2023-11-18 09:58:49 +0300

    feat: add solutions to 7 tasks (15 passing)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -123,8 +123,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value === 0 ? 0 : value % 10;
 }
 
 /**
@@ -155,8 +155,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return (a ** 2 + b ** 2 + c ** 2) ** 0.5;
 }
 
 /**
@@ -176,8 +176,18 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  let realPow = 0;
+  if (pow === 0) {
+    realPow = 1;
+  } else if (pow === 1) {
+    realPow = 10;
+  } else if (pow === 2) {
+    realPow = 100;
+  } else if (pow === 3) {
+    realPow = 1000;
+  }
+  return Math.round(num / realPow) * realPow;
 }
 
 /**
@@ -197,8 +207,13 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n - 1; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -216,8 +231,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) ? Number(value) : def;
 }
 
 /**
@@ -248,8 +263,16 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const fib = [0, 1];
+  if (index === 0) {
+    return 0;
+  }
+
+  for (let i = 2; i <= index; i += 1) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[fib.length - 1];
 }
 
 /**
